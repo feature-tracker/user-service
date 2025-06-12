7a152deb95c7daad0407ae4bd5fd7abc0fcf03d1
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-06-12 09:39:43 +0530

    fix: add security config for user-service
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

---
+++ src/main/java/com/sivalabs/ft/users/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.sivalabs.ft.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(c -> c.requestMatchers(
+                                "/favicon.ico",
+                                "/actuator/**",
+                                "/error",
+                                "/swagger-ui.*",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.*")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
